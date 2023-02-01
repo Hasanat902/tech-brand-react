562b8ab9926cc5d72b285716b0316bc8e25bc9d9
Hasanat902 <abulhasanat902@gmail.com>
2023-02-01 15:04:07 +0600

    added banner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import About from './components/About';
+import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
+      <Hero></Hero>
+      <About></About>
     </>
   );
 }
