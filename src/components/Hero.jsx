c78f8d9c3e02a8891cfa9c99f774fab5fc4785cb
Hasanat902 <abulhasanat902@gmail.com>
2023-02-01 18:15:15 +0600

    ceated about section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,7 +22,7 @@ const Hero = () => {
                 <div>
                     <img className='w-full' src={bgImg} alt="" />
                 </div>
-                <div className='absolute flex flex-col py-8 md:min-w-[760px] bottom-[5%] mx-1 md:left-1/2 transform md:-trans-x-1/2 bg-zinc-200 border border-slate-300 rounded-xl text-center shadow-xl'>
+                <div className='absolute flex flex-col py-8 md:min-w-[760px] bottom-[5%] mx-1 md:left-1/4 transform md:-trans-x-1/2 bg-zinc-200 border border-slate-300 rounded-xl text-center shadow-xl'>
                     <p>Data Services</p>
                     <div className='flex justify-between flex-wrap px-4'>
                         <p className='flex px-4 py-2 text-slate-500'><CloudUploadIcon className='h-6 text-indigo-600'></CloudUploadIcon> App Security</p>
